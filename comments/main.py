a3cfdfbee510e1cefd4db27022b885a943c480d5
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-02-09 18:41:09 +0100

    commiting up to and including strings assignments

---
+++ comments/main.py
@@ -0,0 +1,16 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+
+# first single line comment
+# second single line comment
+print("hello world")  # this prints something
+print("hello world")  # this prints something
+""" multiline 
+comment """
+""" multiline 
+comment """
+print("this string contains a single quote (') character") 
