6412f008a226f2202bd81dd3dbef0940f6a6b233
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-03-25 21:58:21 +0100

    opdracht 3 klaar
    aantal functies uitgecomment om testen simpel te houden

--- files/main.py
+++ files/main.py
@@ -6,6 +6,7 @@ from zipfile import ZipFile
 
 
 # opdracht 1 clean_cache
+# ?mss tempfile mod gebruiken om cache te maken???
 
 
 def clean_cache():
@@ -24,25 +25,19 @@ def clean_cache():
             if len(os.listdir(cache_folder)) == 0:
                 print(f"{cache_folder} directory is empty")
             else:
+                print("Cache directory found.")
                 print(f"removing all files and folders from {cache_folder} directory")
 
             for filename in os.listdir(cache_folder):
                 file_path = os.path.join(cache_folder, filename)
                 if os.path.isdir(file_path):
                     print(f"removing folder {file_path} and its content")
-                    shutil.rmtree(
-                        file_path
-                    )  # removes files/ (not linked files, didnt really understand what those are supposed to be)
+                    shutil.rmtree(file_path)
                 else:
-                    print(f"removing {file_path}")
-                    os.unlink(
-                        file_path
-                    )  # removes all dirs with content and subsequent dirs
+                    os.unlink(file_path)
 
         else:
-            print(
-                f"No cache directory found, creating directory with name {cache_folder}..."
-            )
+            print("No cache directory found, creating new cache directory...")
             os.mkdir(cache_folder)
             print(f"{cache_folder} directory created!")
 
@@ -51,17 +46,52 @@ def clean_cache():
 
 
 # opdracht 2 cache_zip
-cache_folder = os.path.join(os.getcwd(), "cache")
-zipfile_path = os.path.join(os.getcwd(), "data.zip")
 
 
 def cache_zip(zipfile_path, cache_dir_path):
-    clean_cache()  # cleans cache first or creates new cache dir before extracting
-    zipfile_path = os.path.join(cache_folder, zipfile_path)
-    print(zipfile.is_zipfile(zipfile_path), "print if is zipfile")
-
+    """
+    takes a zip file path (str) and a cache dir path (str)
+     as arguments,in that order.
+     The function then unpacks the indicated
+     zip file into a clean cache folder.
+    """
+    clean_cache()
     with ZipFile(zipfile_path) as zipObject:
+        print(f"extracting files into {cache_dir_path}. Please wait... ")
         zipObject.extractall(path=cache_dir_path)
+        print("extraction done!!!")
 
 
-cache_zip(zipfile_path, cache_folder)
+test_zipfile = os.path.join(os.getcwd(), "data.zip")
+cache_folder = os.path.join(os.getcwd(), "cache")
+
+# cache_zip(test_zipfile, cache_folder)
+
+
+# opdracht 3 cached_files
+
+
+def cached_files():
+    """
+    returns a list of all the files in absolute terms located in cache.
+    folders are skipped
+
+    """
+    cache_folder = os.path.join(os.getcwd(), "cache")
+    cache_list = []
+    for filename in os.listdir(cache_folder):
+        file_path = os.path.join(
+            cache_folder, filename
+        )  # zelfde als abs_path ???  waarom?
+        abs_path = os.path.abspath(file_path)
+        if os.path.isdir(file_path):
+            print(f"Folder detected! Skipping folder: {filename}")
+            # continue
+        else:
+            print(f"caching.. {filename}")
+            cache_list.append(abs_path)
+    print("All files cached!")
+    return cache_list
+
+
+cached_files()
